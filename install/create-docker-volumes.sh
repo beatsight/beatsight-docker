cf3212a1baad4b5cee642c91fe465445e6ac50b6
xiez <xiez1989@gmail.com>
2024-10-04 11:02:56 +0800

    wip

--- install/create-docker-volumes.sh
+++ install/create-docker-volumes.sh
@@ -1,6 +1,6 @@
 echo "${_group}Creating volumes for persistent storage ..."
 
-# echo "Created $(docker volume create --name=beatsight-data)."
+echo "Created $(docker volume create --name=beatsight-data)."
 echo "Created $(docker volume create --name=beatsight-redis)."
 
 echo "${_endgroup}"
