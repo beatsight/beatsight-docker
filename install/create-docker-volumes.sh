1ed4d633517653e61b62707861a14464f7d50e86
xiez <xiez1989@gmail.com>
2024-10-12 13:27:40 +0800

    feat: add rabbitmq service

--- install/create-docker-volumes.sh
+++ install/create-docker-volumes.sh
@@ -2,5 +2,6 @@ echo "${_group}Creating volumes for persistent storage ..."
 
 echo "Created $(docker volume create --name=beatsight-data)."
 echo "Created $(docker volume create --name=beatsight-redis)."
+echo "Created $(docker volume create --name=beatsight-mq)."
 
 echo "${_endgroup}"
