a4f2b58e3abb26c20acbcb3e7bcb9d766e53b811
xiez <xiez1989@gmail.com>
2024-11-05 14:32:42 +0800

    chore: remove ansi never of dc

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dc_base="$(docker compose version &>/dev/null && echo 'docker compose' || echo 'docker-compose')"
-dc="$dc_base --ansi never"
+dc="$dc_base"
 
 if [[ -f "../.env" ]]; then
     $dc --env-file .env --env-file ../.env stop
